e22a96f0d32f27e0eeca748714417cb40141ec43
davidtrigo <d.trigo83@gmail.com>
2020-11-23 14:57:03 +0100

    fix html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,50 +9,48 @@ import { environment } from 'src/environments/environment';
   selector: 'app-root',
   template: `
   <header>
-    <h1>{{title}}</h1>
-    <p>Angular application example</p>
-  </header>
-    <form>
-    <legend>
+      <h1>{{ title }}</h1>
+      <p>Angular application example for lift off 🚀</p>
+    </header>
+    <section>
+      <form>
+        <legend>
           Searching {{ queryParams.numberOfLaunches }} launches related to
           {{ queryParams.searchTerm }}
         </legend>
-      <div>
-        <label for="searchTerm">Search term</label>
-        <input name="searchTerm" type="text" [(ngModel)] ="queryParams.searchTerm" />
-      </div>
-      <div>
-        <label for="numberOfLaunches">Number of launches </label>
-        <input name="numberOfLaunches" type="number" [(ngModel)] ="queryParams.numberOfLaunches"/>
-      </div>
-    <button type="submit" (click)="getSpaceData()">Go !</button>
-    </form>
-
-    <section *ngIf="launches.length > 0">
-    <header>
-        <h2>Found {{ launches.length }} launches</h2>
-    </header>
-    
-    <aside *ngFor="let launch of launches" class="{{ launch.status.name | lowercase }}">
-      <h3>
-      {{ launch.name }}
-      </h3>
-      <p>
-        <b>on {{ launch.net | date: 'dd/MM/yyyy HH:mm:ss' }}</b>
-      </p>
-      <p>
-        <i> at {{ launch.location }}</i>
-      </p>
-        <i> pad: {{ launch.pad }}</i>
-    </aside>
+        <div>
+          <label for="searchTerm">Search term</label>
+          <input name="searchTerm" type="text" [(ngModel)]="queryParams.searchTerm" />
+        </div>
+        <div>
+          <label for="numberOfLaunches">Number of launches</label>
+          <input name="numberOfLaunches" type="number" [(ngModel)]="queryParams.numberOfLaunches" />
+        </div>
+        <button type="submit" (click)="getSpaceData()">Go !</button>
+      </form>
     </section>
-
+    <section *ngIf="launches.length > 0">
+      <header>
+        <h2>Found {{ launches.length }} launches</h2>
+      </header>
+      <aside *ngFor="let launch of launches" class="{{ launch.status.name | lowercase }}">
+        <h3>
+          {{ launch.name }}
+        </h3>
+        <p>
+          <b>on {{ launch.net | date: 'dd/MM/yyyy HH:mm:ss' }}</b>
+        </p>
+        <p>
+          <i> at {{ launch.location }}</i>
+        </p>
+        <i> pad: {{ launch.pad }}</i>
+      </aside>
+    </section>
     <aside *ngIf="launches.length == 0">
       <header>
         <h4>📡 Waiting... No data yet 📡</h4>
       </header>
     </aside>
-
     <aside *ngIf="weHaveAproblem !== ''">
       <header>
         <h3>💫 Houston, we have a problem! 💫</h3>
