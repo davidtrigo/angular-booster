40122b5b5194ed5316fa7f4ce6909ff504b68849
davidtrigo <d.trigo83@gmail.com>
2020-11-23 20:57:43 +0100

    updates to heroku

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-booster'));
+app.use(express.static(__dirname+'/dist/angular-booster'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-booster/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/angular-booster/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
